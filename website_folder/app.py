da97ce819fdb5520468ffcba9c427e5d557ab38d
School <School@Jessicas-MacBook-Pro.local>
2025-05-13 10:44:38 -0500

    newpage

--- website_folder/app.py
+++ website_folder/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/newpage")
+def new_page():
+    return render_template("newpage.html")
 
 if __name__ == "__main__":
     app.run()
